4c1573eefc75949dc991718154157cbc87caa578
GAMER 770 <92379700+S70-SCRIPTS@users.noreply.github.com>
2024-12-03 16:34:29 +0530

    Create  fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GAMER 770'
+description 'Discord Rich Presence Script with Player Location'
+version '1.0.0'
+
+client_script 'discord_richpresence.lua'
+
+dependencies {
+    'es_extended' 
+}
